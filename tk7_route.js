14552eda57c04cb4005122ee38acc9ff774bb251
msk2233 <shruti.mokariya.2024@gmail.com>
2024-04-01 14:19:19 +0530

    task 7 routers

---
+++ tk7_route.js
@@ -0,0 +1,22 @@
+var mysql = require('mysql');
+const { execute } = require('./executequery');
+
+exports.tk7route =async (req, res) => {
+    var query = req.query.qr;
+    var pgno = parseInt(req.query.pgno);
+    if (pgno == 1 || pgno == 0) {
+        pgno = 1;
+        x = 0;
+    }
+    else {
+        x = (pgno - 1) * 20;
+    }
+    sqldis = `${query} limit ${x},20`;
+    sql =  `${query}`;
+    data = await execute(sql);
+    Data = await execute(sqldis);
+  
+        var length = Math.ceil(data.length/20);
+        console.log(Data);
+            res.render('tk7_display', { userData: Data, pgno: pgno, length, query});
+}
