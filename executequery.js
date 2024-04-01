28b0966736fe579a2d7b3f349eb7657de244a437
msk2233 <shruti.mokariya.2024@gmail.com>
2024-04-01 13:35:47 +0530

    authenticate

---
+++ executequery.js
@@ -0,0 +1,59 @@
+var mysql = require('mysql')
+
+async function execute(query) {
+    var con = mysql.createConnection({
+        host: "localhost",
+        user: "root",
+        password: "Dev@123",
+        database: "registration",
+        dateStrings : true
+    });
+
+
+    con.connect(function (err) {
+        if (err) throw err;
+    });
+
+    let res = new Promise((resolve, reject) => {
+        con.query(query, (err, result) => {
+            if (err) {
+                console.log(err);
+                reject(err);
+            }
+            else {
+                resolve(result);
+            }
+        })
+    })
+
+    let result = res.then((result)=>{return result}).catch((err)=>{return "Error in query"});
+
+    return result;
+}
+async function insert(query) {
+    var con = mysql.createConnection({
+        host: "localhost",
+        user: "root",
+        password: "Dev@123",
+        database: "registration"
+    });
+
+
+    con.connect(function (err) {
+        if (err) throw err;
+    });
+
+    let res = new Promise((resolve, reject) => {
+        con.query(query, (err, result) => {
+            if (err) {
+                reject(err);
+            }
+            else {
+                resolve(result.insertId);
+            }
+        })
+    })
+    let result = res.then((result)=>{return result}).catch((err)=>{return err});
+    return result;
+}
+module.exports = {execute,insert};
