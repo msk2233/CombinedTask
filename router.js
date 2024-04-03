3f8dc6f9790b314b0534d9af4c3bc574927a60e9
msk2233 <shruti.mokariya.2024@gmail.com>
2024-04-03 19:17:48 +0530

    router file

---
+++ router.js
@@ -0,0 +1,95 @@
+let express = require("express");
+let app = require("express").Router();
+app.use(express.static('public'));
+let cookieParser = require('cookie-parser');
+app.use(cookieParser());
+let auth = require('./middlewear/authentication.js');
+let tk4 = require('./controllers/staticgrid_route.js')
+let tk5 = require('./controllers/Grid_With_Calendar_route.js')
+let tk6 = require('./controllers/resultgrid_route.js')
+let tk7 = require('./controllers/dynemicgrid_route.js')
+let tk8 = require('./controllers/Searching_route.js')
+let tk9 = require('./controllers/Delimitersearch_route.js')
+let tk13 = require('./controllers/job_application_form_route.js')
+let tk14 = require('./controllers/tk14_route.js')
+let tk15 = require('./controllers/timezone_route.js')
+let tk16 = require('./controllers/jsonplaceholder_route.js')
+let reg = require('./controllers/register.js')
+let login = require('./controllers/login.js')
+let rend = require('./controllers/renderpages_route.js')
+
+app.get('/', reg.registerpage)
+app.post('/reg_data', reg.register)
+app.get('/activate_link', reg.activate)
+app.get('/password', reg.getpass)
+app.post('/create_password', reg.createpass)
+app.get('/login', login.login)
+app.get('/password/login', login.login)
+app.post('/login_data', login.login_data)
+app.get('/dashboard', auth, rend.dashboard);
+app.get('/dashboard/dynemictable', auth, rend.dynemictable)
+app.get('/dashboard/Javascript_Events', auth, rend.Javascript_Events)
+app.get('/dashboard/KuKucube', auth, rend.KuKucube)
+app.get('/dashboard/staticgrid', auth, tk4.tk4route)
+app.get('/dashboard/Grid_With_Calendar', auth, tk5.tk5route)
+app.get('/dashboard/result', auth, tk6.tk6route_main)
+app.get('/dashboard/result/detailedresult', auth, tk6.tk6route_detail)
+app.get('/dashboard/dynemicgrid_main', auth, tk7.tk7route_main)
+app.post('/dashboard/dynemicgrid_main/submit', tk7.tk7route_submit)
+app.get('/dashboard/dynemicgrid_main/display', auth, tk7.tk7route)
+app.get('/dashboard/Searching', auth, tk8.tk8route_main)
+app.post('/dashboard/Searching/Searched_data', auth, tk8.tk8route)
+app.get('/dashboard/Delimitersearch', auth, tk9.tk9route_main)
+app.post('/dashboard/Delimitersearch/Delimitersearch_output', auth, tk9.tk9route_display)
+app.get('/dashboard/ehya_tamplate', auth, rend.ehya_tamplate)
+app.get('/dashboard/Awan_Hoster_template', auth, rend.Awan_Hoster_template)
+app.get('/dashboard/hirex_tamplate', auth, rend.hirex_tamplate)
+app.get('/dashboard/job_application_form', auth, tk13.main)
+app.get('/dashboard/job_application_form_main/newform', auth, tk13.insert)
+app.post('/dashboard/job_application_form_main/post_data', auth, tk13.insdata)
+app.get('/dashboard/job_application_form_main/updateform', auth, tk13.update)
+app.get('/fetch', auth, tk13.fetch)
+app.post('/dashboard/job_application_form_main/update', auth, tk13.updatedata)
+app.get('/dashboard/job_application_form_main/get_state', auth, tk13.get_state);
+app.get('/dashboard/job_application_form_main/get_city', auth, tk13.get_city);
+
+app.get('/dashboard/tk14', auth, (req, res) => {
+    res.render('tk14_main')
+})
+app.get('/dashboard/tk14_main/tk14_newform', auth, (req, res) => {
+    tk14.insert(req, res);
+})
+app.post('/dashboard/tk14_main/post_data', auth, (req, res) => {
+    tk14.backend(req, res)
+    if (tk14.backend(req, res)) {
+        tk14.insdata(req, res);
+    }
+    else {
+        res.json(fail);
+    }
+})
+app.get('/dashboard/tk14_main/tk14_updateform', auth, (req, res, next) => {
+    tk14.update(req, res, next);
+})
+app.get('/fetch', auth, async (req, res) => {
+    tk14.fetch(req, res);
+})
+app.post('/dashboard/tk14_main/update', auth, (req, res) => {
+    tk14.backend(req, res)
+    if (tk14.backend(req, res)) {
+        tk14.updatedata(req, res);
+    }
+    else {
+        res.json(fail);
+    }
+})
+app.get('/dashboard/tk14_main/get_state', auth, async function (req, res) {
+    tk14.get_state(req, res);
+});
+app.get('/dashboard/tk14_main/get_city', auth, async function (req, res) {
+    tk14.get_city(req, res);
+});
+app.get('/dashboard/timezone', auth, tk15.tk15route);
+app.get('/dashboard/timezone/converted_timezone', auth, tk15.tk15route_convert);
+app.get('/dashboard/jsonplaceholder', auth, tk16.tk16route);
+module.exports = app;
